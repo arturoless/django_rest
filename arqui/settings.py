7226c1065953e662a6c40a04df3fa6ce07c44633
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-05-22 10:52:12 -0500

    .gitignore is now working

--- arqui/settings.py
+++ arqui/settings.py
@@ -95,17 +95,22 @@ WSGI_APPLICATION = 'arqui.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+#         'NAME' : 'arquidb',
+#         'USER': 'postgres',
+#         'PASSWORD': '14072017',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'arquidb',
-        'USER': 'postgres',
-        'PASSWORD': '14072017',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
