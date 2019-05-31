39e0e9a33301c2489562cf604ff62f6411035328
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-05-31 11:38:22 -0500

    uf

--- arqui/settings.py
+++ arqui/settings.py
@@ -14,7 +14,7 @@ import os
 
 #Heroku
 from decouple import config
-from dj-database-url import *
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # Agregando aplicaciones independientes de Django
     'Login',
+    'Example',
     'rest_framework',
     'rest_framework.authtoken',
 ]
