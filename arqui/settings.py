d33f5cdb6d2f34211b45fe9fa47dcc6780e1b428
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-05-31 18:35:03 -0500

    uf

--- arqui/settings.py
+++ arqui/settings.py
@@ -11,9 +11,6 @@ from decouple import config
 import dj_database_url
 # Importaciones necesarios de heroku fin
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -127,7 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 try:
     from arqui.local_settings import *
