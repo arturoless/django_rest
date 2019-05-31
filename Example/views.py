95e544ae4e3854def3e28180f3d6524928729034
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-05-31 12:14:55 -0500

    uf

--- Example/views.py
+++ Example/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+from rest_framework.views import APIView
 
 # Create your views here.
--- arqui/settings.py
+++ arqui/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 try: 
-    from local_settings import *
+    from arqui.local_settings import *
 except ImportError:
     pass
 
