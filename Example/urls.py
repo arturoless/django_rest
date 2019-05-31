5926992c36efce721a0c811620b1a78464b3f050
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-05-31 18:50:51 -0500

    uf

--- Example/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from django.conf.urls import include
-from django.contrib.auth.models import User
-
-from Example import views
-
-urlpatterns = [
-    re_path(r'example_lista/$', views.ExampleList.as_view()),
-]
