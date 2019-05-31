a1d199a7fef9fe3bc27aca5e02b395bfb7bf0fca
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-05-31 11:29:48 -0500

    uf

--- arqui/settings.py
+++ arqui/settings.py
@@ -14,7 +14,7 @@ import os
 
 #Heroku
 from decouple import config
-import dj-database-url
+from dj-database-url import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -108,12 +108,12 @@ WSGI_APPLICATION = 'arqui.wsgi.application'
 #         'PORT': '5432'
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
