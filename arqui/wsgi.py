8b4cdc90c52549043a24345501235350d35f8506
arturoless <173228@ids.upchiapas.edu.mx>
2019-10-24 12:59:47 -0500

    Update wsgi.py

--- arqui/wsgi.py
+++ arqui/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arqui.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arqui.local_settings')
 
 application = get_wsgi_application()
